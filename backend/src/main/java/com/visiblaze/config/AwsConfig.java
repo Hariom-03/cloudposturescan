83f41579dadb0d03a4d89dc2c73a2137fcd86e99
Hariom-03 <hariompatidar977@gmail.com>
2026-02-14 14:25:52 +0530

    update config

--- backend/src/main/java/com/visiblaze/config/AwsConfig.java
+++ backend/src/main/java/com/visiblaze/config/AwsConfig.java
@@ -3,7 +3,10 @@ package com.visiblaze.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudtrail.CloudTrailClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -17,43 +20,57 @@ public class AwsConfig {
     @Value("${aws.region:us-east-1}")
     private String awsRegion;
 
+    @Value("${aws.access-key:}")
+    private String accessKey;
+
+    @Value("${aws.secret-key:}")
+    private String secretKey;
+
     @Bean
-    public Ec2Client ec2Client() {
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        if (accessKey != null && !accessKey.isBlank() && !accessKey.contains("YOUR_ACCESS_KEY")) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        }
+        return DefaultCredentialsProvider.create();
+    }
+
+    @Bean
+    public Ec2Client ec2Client(AwsCredentialsProvider credentialsProvider) {
         return Ec2Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
     @Bean
-    public S3Client s3Client() {
+    public S3Client s3Client(AwsCredentialsProvider credentialsProvider) {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
     @Bean
-    public IamClient iamClient() {
+    public IamClient iamClient(AwsCredentialsProvider credentialsProvider) {
         return IamClient.builder()
-                .region(Region.AWS_GLOBAL) // IAM is global
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.AWS_GLOBAL)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
     @Bean
-    public CloudTrailClient cloudTrailClient() {
+    public CloudTrailClient cloudTrailClient(AwsCredentialsProvider credentialsProvider) {
         return CloudTrailClient.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
     @Bean
-    public DynamoDbClient dynamoDbClient() {
+    public DynamoDbClient dynamoDbClient(AwsCredentialsProvider credentialsProvider) {
         return DynamoDbClient.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
